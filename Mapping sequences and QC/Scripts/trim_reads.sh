0e06d5ad8e1ad59201a1881cb05f634a5832fa38
Alassane Sow <132286252+AlassaneSow@users.noreply.github.com>
2026-02-08 17:46:15 -0500

    Modify trim_reads.sh for bash and SLURM array
    Updated the script to use bash instead of sh, added SLURM array support, and adjusted thread usage for trimmomatic.

--- Mapping sequences and QC/Scripts/trim_reads.sh
+++ Mapping sequences and QC/Scripts/trim_reads.sh
@@ -1,22 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 #SBATCH --account=plantpath
 #SBATCH --qos=plantpath
 #SBATCH --job-name=trim_reads
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=a.sow@ufl.edu
 #SBATCH --ntasks=1
+#SBATCH --array=1-100
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=12gb
 #SBATCH --time=12:00:00
 #SBATCH --output=trim_reads_%j.out
 pwd; hostname; date
 
+names="path_to_names_file"
+name=$(sed -n "${SLURM_ARRAY_TASK_ID}p" "${names}")
 data="path/to/raw/reads/folder/"
-name="path_to_names_file"
 out="path/to/output/folder"
 adaptor="path/to/adaptor/file.txt"
 
-trimmomatic PE -phred33 -threads 2 \ 
+trimmomatic PE -phred33 -threads 4 \
 ${data}sharedprefix_${name}_R1.fastq.gz \
 ${data}sharedprefix_${name}_R2.fastq.gz \
 ${out}/${name}.R1.trim.fq.gz \
