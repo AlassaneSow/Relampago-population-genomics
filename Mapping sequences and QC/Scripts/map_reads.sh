2435c388730ddfa8c700352023b076f445f7b8c1
Alassane Sow <132286252+AlassaneSow@users.noreply.github.com>
2026-02-09 16:03:11 -0500

    Create map_reads.sh

---
+++ Mapping sequences and QC/Scripts/map_reads.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#SBATCH --account=plantpath
+#SBATCH --qos=plantpath
+#SBATCH --job-name=map_reads
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=
+#SBATCH --ntasks=1
+#SBATCH --array=1-100
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=12gb
+#SBATCH --time=12:00:00
+#SBATCH --output=map_reads_%j.out
+pwd; hostname; date
+
+names="path_to_names_file"
+name=$(sed -n "${SLURM_ARRAY_TASK_ID}p" "${names}")
+ref="path/to/reference.fasta"
+data="path/to/raw/reads/folder"
+OUTDIR="path/to/output/folder"
+TMPDIR="path/to/tempdir/${name}"
+
+mkdir -p "${OUTDIR}"
+mkdir -p "${TMPDIR}"
+
+module load bwa
+module load sambamba
+
+bwa mem -M -t ${SLURM_CPUS_PER_TASK} \
+  -R "@RG\tID:${name}\tSM:${name}\tPL:Illumina" \
+  ${ref} \
+  ${data}/${name}_1.trim.fq.gz \
+  ${data}/${name}_2.trim.fq.gz \
+| sambamba view -t ${SLURM_CPUS_PER_TASK} -S -f bam -l 0 /dev/stdin \
+| sambamba sort /dev/stdin \
+    -t ${SLURM_CPUS_PER_TASK} \
+    -l 0 \
+    -m 10GB \
+    --tmpdir ${TMPDIR} \
+    -o ${OUTDIR}/${name}.sorted.bam
