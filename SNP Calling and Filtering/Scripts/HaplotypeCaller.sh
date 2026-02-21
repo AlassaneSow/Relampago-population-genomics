4b522d38f81174b5a0873458f0c0ceff2c164ec3
Alassane Sow <132286252+AlassaneSow@users.noreply.github.com>
2026-02-20 21:27:16 -0500

    Update and rename Hcall.sh to HaplotypeCaller.sh

--- SNP Calling and Filtering/Scripts/HaplotypeCaller.sh
+++ SNP Calling and Filtering/Scripts/HaplotypeCaller.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
+#SBATCH --account=plantpath
+#SBATCH --qos=plantpath
+#SBATCH --job-name=Hcaller
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=
+#SBATCH --ntasks=1
 #SBATCH --array=1-100
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=16gb
+#SBATCH --time=12:00:00
+#SBATCH --output=Hcaller_%j.out
+pwd; hostname; date
 
 REF="/path_to_reference"
 BAM_LIST="/path_to_bam_list.txt"
 OUT="/path_to_output_gvcf_folder"
-
 BAM=$(sed -n "${SLURM_ARRAY_TASK_ID}p" "$BAM_LIST")
 SAMPLE=$(basename "$BAM" .bam)
+
+module load gatk
 
 gatk HaplotypeCaller \
   -R "${REF}" \
