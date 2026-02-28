8a666b0d572985d50e37a7c389341af299735077
Alassane Sow <132286252+AlassaneSow@users.noreply.github.com>
2026-02-27 20:26:07 -0500

    Update pixy_filter.R

--- Population genetic statistics/Scripts/pixy_filter.R
+++ Population genetic statistics/Scripts/pixy_filter.R
@@ -1,5 +1,24 @@
-data <- read.table("path/to/pixy_output.tsv", header=TRUE, sep="\t")
-fst_cut <- quantile(data$fst, 0.95, na.rm=TRUE)
-dxy_cut <- quantile(data$dxy, 0.95, na.rm=TRUE)
-out <- d[data$fst >= fst_cut & data$dxy >= dxy_cut, ]
-write.table(out, "path/to/pixy/output/5perc_genic.tsv, sep="\t", quote=FALSE, row.names=FALSE)
+library(tidyverse)
+
+fst <- read_tsv("path/to/pixy_fst_output.tsv")
+dxy <- read_tsv("path/to/pixy_dxy_output.tsv")
+genes <- read_tsv("path/to/genes.bed",
+                  col_names = c("chromosome","start","end","gene_id"))
+data <- fst %>%
+  select(chromosome,start,end,pop1,pop2,avg_wc_fst) %>%
+  left_join(
+  dxy %>%
+    select(chromsome, start,end,pop1,pop2,avg_dxy),
+  by = c("chromsome", "start","end","pop1","pop2","avg_dxy")
+)
+data <- data %>%
+  left_join(genes,
+            by = c("chromosome","start","end"))
+divergent_loci <- data %>%
+  group_by(pop1,pop2) %>%
+  filter(
+    avg_wc_fst >= quantile(avg_wc_fst,0.95,na.rm=TRUE),
+    avg_dxy >= quantile(avg_dxy, 0.95, na.rm=TRUE)) %>%
+    ungroup()
+write_tsv(data, "raw_divergent_loci.tsv")
+write_tsv(divergent_loci, "divergent_loci_fst_dxy.tsv")
