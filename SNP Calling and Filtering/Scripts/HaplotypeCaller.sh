70c1748067eb2add317d92bbd3db4dc15868775e
Alassane Sow <132286252+AlassaneSow@users.noreply.github.com>
2026-04-02 13:22:18 -0400

    Update HaplotypeCaller script parameters and paths

--- SNP Calling and Filtering/Scripts/HaplotypeCaller.sh
+++ SNP Calling and Filtering/Scripts/HaplotypeCaller.sh
@@ -6,22 +6,22 @@
 #SBATCH --mail-user=
 #SBATCH --ntasks=1
 #SBATCH --array=1-100
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=1
 #SBATCH --mem=16gb
-#SBATCH --time=12:00:00
+#SBATCH --time=6:00:00
 #SBATCH --output=Hcaller_%j.out
 pwd; hostname; date
 
-REF="/path_to_reference"
-BAM_LIST="/path_to_bam_list.txt"
-OUT="/path_to_output_gvcf_folder"
-BAM=$(sed -n "${SLURM_ARRAY_TASK_ID}p" "$BAM_LIST")
-SAMPLE=$(basename "$BAM" .bam)
+REF="/path/to/reference.fna"
+names="/path/to/names.txt"
+OUT="path/to/snp_calling_folder"
+name=$(sed -n "${SLURM_ARRAY_TASK_ID}p" "${names}")
+BAM="/path/to/filtered_assemblies"
 
 module load gatk
 
 gatk HaplotypeCaller \
-  -R "${REF}" \
-  -I "${BAM}" \
+  -R ${REF} \
+  -I ${BAM}/${name}_sort_20_dup.bam \
   -ERC GVCF \
-  -O "${OUT}/${SAMPLE}.g.vcf.gz"
+  -O "${OUT}/${name}.g.vcf.gz"
