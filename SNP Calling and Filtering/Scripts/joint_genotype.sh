2322e63763bb781070d353561ba1aab02c93f590
Alassane Sow <132286252+AlassaneSow@users.noreply.github.com>
2026-04-07 15:41:22 -0400

    Update SLURM script for joint genotype job

--- SNP Calling and Filtering/Scripts/joint_genotype.sh
+++ SNP Calling and Filtering/Scripts/joint_genotype.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
-#SBATCH --account=
-#SBATCH --qos=
+#SBATCH --account=plp6235
+#SBATCH --qos=plp6235-b
 #SBATCH --job-name=joint_genotype
 #SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=
+#SBATCH --mail-user=a.sow@ufl.edu
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=16gb
-#SBATCH --time=12:00:00
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=50gb
+#SBATCH --time=48:00:00
 #SBATCH --output=joint_genotype_%j.out
 pwd; hostname; date
 
-REF="/path_to_reference"
-cohort="path/to/cohort_db"
-OUTDIR="path/to/joint/genotype/output"
-mkdir -p $OUTDIR
-module load gatk
+cd 
+ml gatk
 
 gatk GenotypeGVCFs \
-  -R ${REF} \
-  -V gendb://${cohort} \
-  -O ${OUTDIR}/cohort.vcf.gz
+-R /blue/plp6235/asow/ref/reference.fna \
+-V gendb://cohort_db \
+--include-non-variant-sites \
+-O /blue/plp6235/asow/snp_calling/snps/raw_genotypes.vcf.gz
