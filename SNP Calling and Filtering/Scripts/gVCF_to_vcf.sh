a03bd9499078795d0dc621294ed90ab0bc114ca4
Alassane Sow <132286252+AlassaneSow@users.noreply.github.com>
2026-04-07 15:42:21 -0400

    Update SLURM job parameters and paths in script

--- SNP Calling and Filtering/Scripts/gVCF_to_vcf.sh
+++ SNP Calling and Filtering/Scripts/gVCF_to_vcf.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-#SBATCH --account=plantpath
-#SBATCH --qos=plantpath
-#SBATCH --job-name=gVCF_to_vcf
+#SBATCH --account=plp6235
+#SBATCH --qos=plp6235-b
+#SBATCH --job-name=combine_gvcf
 #SBATCH --mail-type=END,FAIL
-#SBATCH --mail-user=
+#SBATCH --mail-user=a.sow@ufl.edu
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --mem=16gb
-#SBATCH --time=12:00:00
-#SBATCH --output=gVCF_to_vcf_%j.out
+#SBATCH --mem=150gb
+#SBATCH --time=48:00:00
+#SBATCH --output=combine_gvcf_%j.out
 pwd; hostname; date
 
-REF="/path_to_reference"
-gvcf_map="path/to/gvcf_map.txt"
-
-module load gatk
+cd /blue/plp6235/asow/Fomotopsis/snp_calling
+ml gatk
 
 gatk GenomicsDBImport \
--R ${REF} \
---genomicsdb-workspace-path cohort_db \
---sample-name-map ${gvcf_map} \
+-R /blue/plp6235/asow/Fomotopsis/ref/fomotopsis_reference.fna \
+--genomicsdb-workspace-path fomotopsis_cohort_db \
+-L /blue/plp6235/asow/Fomotopsis/ref/genome_intervals.list \
+--merge-input-intervals true \
+--sample-name-map gvcf_map.txt
