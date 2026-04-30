430c58c65c6b11c9af382e2494e63604eb18ec8d
Alassane Sow <132286252+AlassaneSow@users.noreply.github.com>
2026-04-30 08:55:19 -0400

    Refactor SNP filtering script and output handling
    Updated script to filter and process SNP data, including changes to output file names and filtering criteria.

--- SNP Calling and Filtering/Scripts/filter_seperate_filter_combine.sh
+++ SNP Calling and Filtering/Scripts/filter_seperate_filter_combine.sh
@@ -8,7 +8,7 @@
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=50gb
 #SBATCH --time=30:00:00
-#SBATCH --output=filter_seperate_allsites_snps_%j.out
+#SBATCH --output=extract_and_qc_%j.out
 pwd; hostname; date
 
 REF="/blue/plp6235/asow/Fomotopsis/ref/fomotopsis_reference.fna"
@@ -20,51 +20,35 @@ ml samtools
 ml vcftools
 ml bcftools 
 
-##Broad filters to remove sites with extremley high & low depth and missingness. I need to see if 75 reasonable for invariant sites in my data.
+## Filter based on depth and missingness #max missing can min DP can be relaxed if needed.
 vcftools --gzvcf ${RAW} \
---remove-indels \
 --max-missing 0.95 \
 --min-meanDP 10 \
 --max-meanDP 75 \
---recode --stdout | bgzip -c > ${OUT}/missingness_filtered_ALLSITES.vcf.gz
+--recode --stdout | bgzip -c > ${OUT}/CLEAN_ALLSITES.vcf.gz
 
-##Extract ONLY SNPs and filter them based on quality
-vcftools --gzvcf ${OUT}/missingness_filtered_ALLSITES.vcf.gz \
---mac 1 \
---recode --stdout | bgzip -c > ${OUT}/missingness_filtered_SNPS.vcf.gz
+tabix -p vcf ${OUT}/CLEAN_ALLSITES.vcf.gz
 
-gatk VariantFiltration \
--R ${REF} \
--V ${OUT}/missingness_filtered_SNPS.vcf.gz \
--O ${OUT}/quality_labeled_SNPS.vcf.gz \
---filter "MQ < 40.0" --filter-name "MQ" \
---filter "FS > 60.0" --filter-name "FS" \
---filter "QD < 2" --filter-name "QD"
-
-bcftools view -f PASS ${OUT}/quality_labeled_SNPS.vcf.gz -Oz -o ${OUT}/quality_filtered_SNPS.vcf.gz
-
-##Extract ONLY BI-ALLELIC SNPs
-vcftools --gzvcf ${OUT}/quality_filtered_SNPS.vcf.gz \
---min-alleles 2 \
---max-alleles 2 \
---recode --stdout | bgzip -c > ${OUT}/quality_filtered_BIALLELIC_SNPS.vcf.gz
-
-##Extract ONLY invariant sites
+## Extract ONLY BIALLELIC SNPs
 gatk SelectVariants \
   -R ${REF} \
-  -V ${OUT}/missingness_filtered_ALLSITES.vcf.gz \
-  --select-type-to-include NO_VARIATION \
-  -O ${OUT}/quality_filtered_INVARIANT.vcf.gz
+  -V ${RAW} \
+  --select-type-to-include SNP \
+  --restrict-alleles-to BIALLELIC \
+  -O ${OUT}/RAW_BIALLELIC_SNPS.vcf.gz
 
 ##Index files, combine, and sort
 cd ${OUT}
-tabix -p vcf quality_filtered_INVARIANT.vcf.gz
-tabix -p vcf quality_filtered_SNPS.vcf.gz
+#tabix -p vcf CLEAN_ALLSITES.vcf.gz
+#tabix -p vcf RAW_BIALLELIC_SNPS.vcf.gz
 
-bcftools concat \
---allow-overlaps \
-quality_filtered_INVARIANT.vcf.gz quality_filtered_SNPS.vcf.gz \
--O z -o quality_filtered_ALLSITES.vcf.gz
+bcftools sort CLEAN_ALLSITES.vcf.gz -Oz -o pixy_input_CLEAN_ALLSITES.vcf.gz
+bcftools index pixy_input_CLEAN_ALLSITES.vcf.gz
+X=$(bcftools view -H pixy_input_CLEAN_ALLSITES.vcf.gz | wc -l)
+echo "There are $X sites in this file"
 
-bcftools sort quality_filtered_ALLSITES.vcf.gz -Oz -o pixy_input_quality_filtered_ALLSITES.vcf.gz
-bcftools index pixy_input_quality_filtered_ALLSITES.vcf.gz
+bcftools sort RAW_BIALLELIC_SNPS.vcf.gz -Oz -o BIALLELIC_SNPS.vcf.gz
+bcftools index BIALLELIC_SNPS.vcf.gz
+Y=$(bcftools view -H BIALLELIC_SNPS.vcf.gz | wc -l)
+echo "There are $Y sites in this file"
+
