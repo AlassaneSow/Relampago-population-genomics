2eafe4658682e886e27ff6a67b75bf3b2e83c230
Alassane Sow <132286252+AlassaneSow@users.noreply.github.com>
2026-06-24 11:27:40 -0400

    Add SLURM job script for estimating projections

---
+++ Demographic modeling/Scripts/estimate_projections.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --account=plantpath
+#SBATCH --qos=plantpath
+#SBATCH --job-name=estimate_projections
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=a.sow@ufl.edu
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=16gb
+#SBATCH --time=4:00:00
+#SBATCH --output=estimate_projections_%j.out
+pwd; hostname; date
+
+VCF="/blue/matthewsmith/a.sow/Sebastian_Data/snps/filtered_snps/LD_PRUNED_BIALLELIC_SNPS.vcf.gz"
+OUT="/blue/matthewsmith/a.sow/Sebastian_Data/demographic_modelling/"
+ml easysfs
+cd ${OUT}
+easysfs -i ${VCF} -p populations.txt -a -f --preview
