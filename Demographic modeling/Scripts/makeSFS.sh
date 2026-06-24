52761db99921598f83f71009f17898e53f6063b6
Alassane Sow <132286252+AlassaneSow@users.noreply.github.com>
2026-06-24 11:55:43 -0400

    Add SLURM script for generating site frequency spectrum

---
+++ Demographic modeling/Scripts/makeSFS.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --account=plantpath
+#SBATCH --qos=plantpath
+#SBATCH --job-name=makeSFS
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=a.sow@ufl.edu
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=16gb
+#SBATCH --time=4:00:00
+#SBATCH --output=makeSFS_%j.out
+pwd; hostname; date
+
+VCF="/blue/matthewsmith/a.sow/Sebastian_Data/snps/filtered_snps/LD_PRUNED_BIALLELIC_SNPS.vcf.gz"
+OUT="/blue/matthewsmith/a.sow/Sebastian_Data/demographic_modelling/"
+POP="/blue/matthewsmith/a.sow/Sebastian_Data/snps/filtered_snps/samples_pop.txt"
+ml easysfs
+cd ${OUT}
+easysfs -i ${VCF} -p ${POP} -a -f --proj 8,8
